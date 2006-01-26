8e664ef73201cff6fd95b08ec2953fde81aa8b01
Anne-Marie Mahfouf <annma@kde.org>
2006-01-26 02:56:13 +0000

    fix compilation fix FSF address
    svn path=/branches/work/kde4/playground/edu/kard/; revision=502459

--- src/kard.cpp
+++ src/kard.cpp
@@ -12,7 +12,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA 02110-1301, USA.
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
 //Qt headers
@@ -33,8 +33,6 @@
 #include "kard.h"
 #include "kardview.h"
 // Settings
-#include "theme.h"
-#include "timer.h"
 #include "kardsettings.h"
 
 Kard::Kard() : KMainWindow( 0,"Kard" ), m_view(new KardView(this))
