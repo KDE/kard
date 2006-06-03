5652c4af0a651607463e95f392ba0f06f0b56de6
Laurent Montel <montel@kde.org>
2006-06-03 12:51:19 +0000

    Extract message
    svn path=/branches/work/kde4/playground/edu/kard/; revision=547796

---
+++ src/Messages.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+$EXTRACTRC *.ui *.rc > rc.cpp
+LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp #-o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`; 
+if test -n "$LIST"; then 
+    $XGETTEXT $LIST -o $podir/kard.pot; 
+fi
+
+
