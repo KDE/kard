a9a89bfd29ffe8d73d54e98dc490ed3f2730e987
Stephan Kulow <coolo@kde.org>
2007-06-10 10:02:34 +0000

    fix syntax
    svn path=/trunk/playground/edu/kard/; revision=673458

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,8 +1,7 @@
 #! /usr/bin/env bash
-$EXTRACTRC *.ui *.rc > rc.cpp
-LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp #-o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`; 
+LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
 if test -n "$LIST"; then 
-    $XGETTEXT $LIST -o $podir/kard.pot; 
+    $XGETTEXT $LIST -o $podir/kard.pot
 fi
 
 
