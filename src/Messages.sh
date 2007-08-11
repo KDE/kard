3848a03859895e1a3eafaf5b849533f5b814da7d
Laurent Montel <montel@kde.org>
2007-08-11 09:09:01 +0000

    fix extract message
    svn path=/trunk/playground/edu/kard/; revision=698844

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,7 +1,4 @@
 #! /usr/bin/env bash
-LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
-if test -n "$LIST"; then 
-    $XGETTEXT $LIST -o $podir/kard.pot
-fi
+$XGETTEXT `find . -name\*.cpp` -o $podir/kard.pot
 
 
